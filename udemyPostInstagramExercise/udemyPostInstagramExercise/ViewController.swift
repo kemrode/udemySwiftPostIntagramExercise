7b3899b4e58d648e2c05aa22de20ec1f4316e199
Kevin Fichou <kevin.fichou@budget-box.com>
2021-06-24 22:28:50 +0200

    feat(main): exercise done

---
+++ udemyPostInstagramExercise/udemyPostInstagramExercise/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  udemyPostInstagramExercise
+//
+//  Created by Kevin Fichou on 24/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //@IBOutlet from View
+    @IBOutlet weak var profilPicture: UIImageView!
+    @IBOutlet weak var faceOnePicture: UIImageView!
+    @IBOutlet weak var faceTwoPicture: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        roundedImg(image: profilPicture)
+        roundedImg(image: faceOnePicture)
+        roundedImg(image: faceTwoPicture)
+    }
+    //function to rounded Image sent
+    private func roundedImg(image: UIImageView!){
+        image.layer.cornerRadius = image.frame.size.width/2
+        image.clipsToBounds = true
+    }
+}
+
